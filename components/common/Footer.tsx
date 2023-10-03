3bd9d53d6911f05cdff15e0cc1d157b6eb3ccb29
Karikari1234! <naafizrahman1@gmail.com>
2023-10-03 16:08:05 +0600

    Update footer

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -1,9 +1,33 @@
+import Link from "next/link";
+import Image from "next/image";
+
 const Footer = () => {
-    return (
+  return (
+    <footer className="bg-green text-white absolute bottom-0 w-full">
+      <div className="p-4 py-6 lg:py-8">
         <div>
-            Enter
+          <div className="flex-col justify-center">
+            <Link href="/">
+              <Image
+                src="/images/site-logo.png"
+                alt="BPDB logo"
+                width={100}
+                height={100}
+              ></Image>
+            </Link>
+            <div className="text-sm font-extralight">
+              Prepaid Metering System has been a revolution for improving the
+              power sector in Bangladesh. BPDB, with its aim to empower people
+              with quality electricity, aims for 100% prepaid metering in near
+              future.
+            </div>
+          </div>
+          <div></div>
         </div>
-    );
-}
+        <div className="text-xs">Built by BPDB. &copy; Copyright 2023.</div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
