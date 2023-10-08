bb28ddaeb875e0d73a9af49a881bf5695ee659f4
Karikari1234! <naafizrahman1@gmail.com>
2023-10-08 12:28:44 +0600

    Fix Styling And Add Animation Hero Section

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -19,24 +19,24 @@ const HeroSection = () => {
       </div>
       <div className="">
         <h1
-          className={`${poppins.className}  font-bold  text-4xl text-center leading-normal mb-5 md:text-6xl md:text-left md:leading-tight`}
+          className={`${poppins.className}  font-bold  text-4xl text-center leading-normal mb-5 md:text-6xl md:text-left md:leading-tight animate-fade-up animate-once`}
         >
           Welcome to <span className="whitespace-nowrap">Prepaid Metering</span>{" "}
           System BPDB
         </h1>
-        <p className="text-center leading-normal text-xl mb-5 md:text-left">
+        <p className="text-center leading-normal text-xl mb-5 md:text-left animate-fade-up animate-once">
           The smart and convenient way to pay for your electricity. With prepaid
           metering, you can monitor your energy consumption, and recharge your
           balance easily.
         </p>
         <div className="flex gap-1 justify-center md:justify-normal md:gap-6">
           <Link href="/">
-            <button className="bg-green border border-green hover:border-transparent hover:bg-green-deep text-white font-bold py-2 px-4 rounded">
+            <button className="bg-green border border-green hover:border-transparent hover:bg-green-deep text-white py-2 px-4 rounded">
               Meter Charges
             </button>
           </Link>
           <Link href="/path">
-            <button className="bg-transparent hover:bg-green text-green font-bold hover:text-white py-2 px-4 border border-green hover:border-transparent rounded">
+            <button className="bg-transparent hover:bg-green text-green hover:text-white py-2 px-4 border border-green hover:border-transparent rounded">
               Meter Short Codes
             </button>
           </Link>
