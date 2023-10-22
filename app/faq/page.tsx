147e4cb2b14fc3066bbbc6be35e2eb7e676544d2
Karikari1234! <naafizrahman1@gmail.com>
2023-10-22 13:20:36 +0600

    Add FAQ to home

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -3,6 +3,9 @@ import FAQAccordion from "@/components/FAQ";
 const FAQ = () => {
   return (
     <div className="md:mx-auto md:w-5/6">
+      <h1 className="heading-text mb-4 !text-left !text-5xl md:mb-8">
+        Frequently Asked Questions
+      </h1>
       <FAQAccordion></FAQAccordion>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import AboutComponent from "@/components/About";
 import CustomerOrderInformationCard from "@/components/CustomerOrderInformationCard";
 import MyComponent from "@/components/DummyDataFetch";
+import FAQAccordion from "@/components/FAQ";
 import HeroSection from "@/components/HeroSection";
 
 export default function Home({
@@ -12,6 +13,13 @@ export default function Home({
     <div className="flex flex-col space-y-16">
       <HeroSection />
       <AboutComponent />
+      <h1 className="heading-text mb-4 !text-center !text-5xl md:mb-8">
+        Frequently Asked Questions
+      </h1>
+      <div className="md:mx-auto md:w-5/6">
+        <FAQAccordion></FAQAccordion>
+      </div>
+
       {/* <CustomerOrderInformationCard /> */}
     </div>
   );
