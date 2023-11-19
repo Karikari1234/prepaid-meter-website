64fcec3901fc8780bc94b6df4d794b176c059dda
Karikari1234! <naafizrahman1@gmail.com>
2023-11-19 11:08:49 +0600

    Update copy mod nav

--- lib/nav-menu.ts
+++ lib/nav-menu.ts
@@ -6,7 +6,10 @@ interface MenuItem {
 export const topMenu: Array<MenuItem> = [
   { title: "Home", route: "/" },
   { title: "About", route: "/about" },
-  { title: "BPDB Issue Tracking", route: "http://180.211.137.8/login" },
+  {
+    title: "BPDB Issue Tracking",
+    route: "http://issuetracker.bpdbservices.net/login",
+  },
   { title: "Calculate Meter Charges", route: "/bill-calculator" },
   { title: "Check Meter Token", route: "/check-token" },
   { title: "FAQ", route: "/faq" },
