ff49cf24c6d39c7bcd0e7ab1b4ca037d5122de5b
Karikari1234! <naafizrahman1@gmail.com>
2023-12-03 12:30:15 +0600

    Remove Server Actions

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const nextConfig = {
         TEST_API_URL: process.env.TEST_API_URL,
         NEXT_PUBLIC_RECAPTCHA_SITE_KEY: process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY,
         RECAPTCHA_SECRET_KEY: process.env.RECAPTCHA_SECRET_KEY
-      },
-      experimental: {
-        serverActions: true,
       }
 }
 
