c94307e5799f2413a327ad40c404683014118f70
Karikari1234! <naafizrahman1@gmail.com>
2023-12-04 13:19:24 +0600

    Update experimental serverActions

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
         TEST_API_URL: process.env.TEST_API_URL,
         NEXT_PUBLIC_RECAPTCHA_SITE_KEY: process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY,
         RECAPTCHA_SECRET_KEY: process.env.RECAPTCHA_SECRET_KEY
+      },
+      experimental: {
+        serverActions: {
+          allowedOrigins: ['180.211.137.7:90']
+        }
       }
 }
 
