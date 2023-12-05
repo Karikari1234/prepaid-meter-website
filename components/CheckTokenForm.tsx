97a1f287f7a0bab45dc757caa60fa668db7b6132
Karikari1234! <naafizrahman1@gmail.com>
2023-12-05 11:42:02 +0600

    Add spinner in check token form

--- components/CheckTokenForm.tsx
+++ components/CheckTokenForm.tsx
@@ -122,8 +122,18 @@ export function CheckInputForm() {
           <Button
             className="w-full bg-green"
             type="submit"
-            disabled={!isVerified}
+            disabled={!isVerified || form.formState.isSubmitting}
           >
+            {form.formState.isSubmitting && (
+              <div
+                className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                role="status"
+              >
+                <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+                  Loading...
+                </span>
+              </div>
+            )}
             Submit
           </Button>
         </div>
