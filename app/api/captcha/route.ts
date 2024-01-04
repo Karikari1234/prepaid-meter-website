cfbf4be8e13f4d9505f576157ff22da143ec8ed0
Karikari1234! <naafizrahman1@gmail.com>
2024-01-04 14:29:14 +0600

    another commit

--- app/api/captcha/route.ts
+++ app/api/captcha/route.ts
@@ -1,5 +1,25 @@
 import { NextRequest, NextResponse } from "next/server";
 
+//const { AlgebraicCaptcha } = require("algebraic-captcha");
+
+// async function generateCaptcha() {
+//   const algebraicCaptcha = new AlgebraicCaptcha({
+//     width: 200,
+//     height: 200,
+//     background: "#ffffff",
+//     noise: 1,
+//     minValue: 1,
+//     maxValue: 10,
+//     operandAmount: 1,
+//     operandTypes: ["+", "-"],
+//     mode: "formula",
+//     targetSymbol: "?",
+//   });
+
+//   const { image, answer } = await algebraicCaptcha.generateCaptcha();
+//   return { image, answer };
+// }
 export async function GET(request: NextRequest) {
+  //const { image, answer } = await generateCaptcha();
   return NextResponse.json({ name: "Karim" });
 }
