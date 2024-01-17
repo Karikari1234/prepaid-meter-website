cb0b7c5f0662db97de534f7de07d1dcc81a9fb78
Karikari1234! <naafizrahman1@gmail.com>
2024-01-17 15:52:22 +0600

    Update next config file

--- next.config.js
+++ next.config.js
@@ -1,15 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        TEST_API_URL: process.env.TEST_API_URL,
-        NEXT_PUBLIC_RECAPTCHA_SITE_KEY: process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY,
-        RECAPTCHA_SECRET_KEY: process.env.RECAPTCHA_SECRET_KEY
-      },
-      experimental: {
-        serverActions: {
-          allowedOrigins: ['127.0.0.1:3000','info.bpdbservices.net']
-        }
-      }
-}
+  env: {
+    TEST_API_URL: process.env.TEST_API_URL,
+    NEXT_PUBLIC_RECAPTCHA_SITE_KEY: process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY,
+    RECAPTCHA_SECRET_KEY: process.env.RECAPTCHA_SECRET_KEY,
+  },
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "127.0.0.1:3000",
+        "info.bpdbservices.net",
+        "prepaid.bpdbservices.net",
+        "127.0.0.1:3001",
+      ],
+    },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
